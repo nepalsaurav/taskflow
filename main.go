a868a4f50d2afe5b9dca567dd034dbb39f4d3b18
nepalsaurav <nepalsaurav123@gmail.com>
2025-12-30 16:54:29 +0545

    add html to pdf converstion initiation

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import "github.com/nepalsaurav/taskflow/models"
-
 func main() {
-
-	// maildir := pkg.Maildir{}
-
-	// watcher, err := fsnotify.NewWatcher()
-	// if err != nil {
-	// 	log.Printf("error while opening watcher for maildir, err: %v\n", err)
-	// }
-	// defer watcher.Close()
-
-	// maildir.WatchMailDir(watcher)
-
-	// // block main go routine forever
-	// <-make(chan struct{})
-	//
-	//
-
-	models.Migrate()
 
 }
